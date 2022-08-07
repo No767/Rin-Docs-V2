18754845b1ff484ec1df0afd469c4f311e994a94
No767 <73260931+No767@users.noreply.github.com>
2022-08-07 02:05:21 -0700

    update configs

--- rin-docs/docusaurus.config.js
+++ rin-docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Rin Docs',
   tagline: 'The official documentation of Rin',
-  url: 'https://rinbot.live',
+  url: 'https://docs.rinbot.live',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'No767', // Usually your GitHub org/user name.
-  projectName: '', // Usually your repo name.
+  projectName: 'Rin-Docs-V2', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/No767/Rin-Docs-V2/tree/master/rin-docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
